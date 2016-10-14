0589e4d4886386d9bdff892b620b2c3cac221502
Linus Brogan <linusaurusrex@users.noreply.github.com>
2016-10-14 16:30:04 -0700

    Make JS run

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-//On page load event listener
-document.addEventListener("DOMContentLoaded", function () {
-    backgroundGen(key); /*Calls background function*/ 
-})
-function backgroundGen(k) {
+
+function backgroundGen() {
     //If everything works out change color
     var key;
     var tempInt = Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
     key = (tempInt.length < 6) ? "0".repeat(6 - tempInt.length) + tempInt : tempInt;
     document.body.bgColor = "#" + key;
 }
+
+backgroundGen();
